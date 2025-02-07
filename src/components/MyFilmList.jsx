145f905c8b23e5dc34eda0347ecbda5667e3faf7
St4n1slav <stanyyaro@gmail.com>
2025-02-07 16:56:29 +0100

    1

--- src/components/MyFilmList.jsx
+++ src/components/MyFilmList.jsx
@@ -16,7 +16,7 @@ class MyFilmList extends Component {
 
       const response = await fetch(`http://www.omdbapi.com/?s=${this.props.filmName}&apikey=72b6ee4a`);
       const json = await response.json();
-      this.setState({ films: json.Search });
+      this.setState({ films: json.Search.slice(0, 5) });
     } catch (error) {
       console.error("Errore nel reperimento dei dati: ", error);
       this.setState({ hasError: true, errorMessage: error.message });
@@ -37,7 +37,7 @@ class MyFilmList extends Component {
     }
     return (
       <>
-        <h5>{this.props.filmName}</h5>
+        <h5 className="filmTitle">{this.props.filmName}</h5>
         <Row className="my-3" sm={2} md={4} xl={6}>
           {this.state.films.map((src) => (
             <MyFilm key={src.imdbID} filmId={src.imdbID} />
